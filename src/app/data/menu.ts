10064e2f08ca98f4f14e98e08b1d330d1274361c
matthewholmen <matthewholmen@gmail.com>
2025-01-07 17:10:45 -0800

    expanded menu

--- src/app/data/menu.ts
+++ src/app/data/menu.ts
@@ -1,67 +1,144 @@
 export const menuData = [
-    {
-      course: "welcome bite",
-      name: "gruyere gougere, savory whipped cream, caraway, dill",
-      price: 5
-    },
-    {
-      course: "welcome bite",
-      name: "candied nut cone",
-      price: 5
-    },
-    {
-      course: "welcome bite",
-      name: "duck confit brioche toast, pickled berries, chervil",
-      price: 5
-    },
-    {
-      course: "supper club grazing table",
-      name: "kyle's crackers, smoked whitefish salad, wisconsin cured meats, pickles, mustards",
-      price: 13
-    },
-    {
-      course: "supper club grazing table",
-      name: "wisconsin cheeses, jams, apple butter, local honey",
-      price: 10
-    },
-    {
-      course: "supper club grazing table",
-      name: "soft pretzel bites, pub cheese",
-      price: 5
-    },
-    {
-      course: "supper club grazing table",
-      name: "supper club crudites, french onion dip",
-      price: 5
-    },
-    {
-      course: "accouterments",
-      name: "warmed marinated olives",
-      price: 4
-    },
-    {
-      course: "accouterments",
-      name: "white bean dip, roasted garlic, herbs",
-      price: 5
-    },
-    {
-      course: "accouterments",
-      name: "deviled eggs, lake superior roe",
-      price: 6
-    },
-    {
-      course: "main",
-      name: "brick oven superior whitefish, lemon caper butter, shoestring potatoes",
-      price: 30
-    },
-    {
-      course: "main",
-      name: "smoked, grilled pork loin, apple butter vinegar",
-      price: 30
-    },
-    {
-      course: "main",
-      name: "prime rib, jus, compound butter",
-      price: 37
-    }
-  ];
+  // Welcome Bites
+  {
+    course: "welcome bite",
+    name: "gruyere gougere, savory whipped cream, caraway, dill",
+    price: 5
+  },
+  {
+    course: "welcome bite",
+    name: "candied nut cone",
+    price: 5
+  },
+  {
+    course: "welcome bite",
+    name: "duck confit brioche toast, pickled berries, chervil",
+    price: 5
+  },
+
+  // Supper Club Grazing Table
+  {
+    course: "supper club grazing table",
+    name: "kyle's crackers, smoked whitefish salad, wisconsin cured meats, pickles, mustards",
+    price: 13
+  },
+  {
+    course: "supper club grazing table",
+    name: "wisconsin cheeses, jams, apple butter, local honey",
+    price: 10
+  },
+  {
+    course: "supper club grazing table",
+    name: "soft pretzel bites, pub cheese",
+    price: 5
+  },
+  {
+    course: "supper club grazing table",
+    name: "supper club crudites, french onion dip",
+    price: 5
+  },
+
+  // At The Table
+  {
+    course: "at the table",
+    name: "our focaccia, with butter, oil and vinegar",
+    price: 6
+  },
+
+  // Accouterments
+  {
+    course: "accouterments",
+    name: "warmed marinated olives",
+    price: 4
+  },
+  {
+    course: "accouterments",
+    name: "white bean dip, roasted garlic, herbs",
+    price: 5
+  },
+  {
+    course: "accouterments",
+    name: "deviled eggs, lake superior roe",
+    price: 6
+  },
+
+  // First Course
+  {
+    course: "first course",
+    name: "fried sweetbreads, cherry tomatoes, black garlic, arugula pesto",
+    price: 15
+  },
+  {
+    course: "first course",
+    name: "shrimp cocktail skewers, cocktail sauce, horseradish",
+    price: 12
+  },
+
+  // Second Course
+  {
+    course: "second course",
+    name: "braised, grilled radicchio, roasted pickled beet, dill, mustard",
+    price: 10
+  },
+  {
+    course: "second course",
+    name: "pickled zucchini, muskmelon, basil",
+    price: 10
+  },
+
+  // Mid Course
+  {
+    course: "mid course",
+    name: "tortellini en brodo, chicken stock, aged parm",
+    price: 15
+  },
+  {
+    course: "mid course",
+    name: "semolina dumplings, adjeer, feta, garden herbs",
+    price: 15
+  },
+  {
+    course: "mid course",
+    name: "sweet corn risotto, roasted mushroom, crispy shallot",
+    price: 16
+  },
+
+  // Main Course
+  {
+    course: "main",
+    name: "brick oven superior whitefish, lemon caper butter, shoestring potatoes",
+    price: 30
+  },
+  {
+    course: "main",
+    name: "smoked, grilled pork loin, apple butter vinegar",
+    price: 30
+  },
+  {
+    course: "main",
+    name: "prime rib, jus, compound butter",
+    price: 37
+  },
+
+  // Vegetable Sides
+  {
+    course: "vegetable sides",
+    name: "garden caponata, eggplant, fried herbs",
+    price: 8
+  },
+  {
+    course: "vegetable sides",
+    name: "butter roasted mushrooms and shallots, thyme, banyuls vinegar",
+    price: 9
+  },
+  {
+    course: "vegetable sides",
+    name: "grilled hakurei turnips, golden raisins, pomegranate molasses",
+    price: 8
+  },
+  {
+    course: "vegetable sides",
+    name: "tender lettuces, puffed rice, oxymel dressing",
+    price: 6
+  }
+];
